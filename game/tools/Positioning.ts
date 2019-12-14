63ecf74571609a75d708842f055705070d48e984
auroratide <tf.auroratide@gmail.com>
2019-12-14 07:57:04 -0600

    Stop logging position

--- game/tools/Positioning.ts
+++ game/tools/Positioning.ts
@@ -14,6 +14,5 @@ export default class Positioning {
     const angle = Math.random() * Math.PI * 2
     const distance = Math.max(this.renderer.width, this.renderer.height) * 0.70914 // sqrt(2) / 2
     obj.position.set(Math.cos(angle) * distance + this.renderer.width / 2, Math.sin(angle) * distance + this.renderer.height / 2)
-    console.log(obj.position)
   }
 }
