01c569929cf962ccc4a2f96325af184fbe26ab74
auroratide <tf.auroratide@gmail.com>
2019-12-15 23:08:49 -0600

    load the font before the game starts

--- game/index.ts
+++ game/index.ts
@@ -2,34 +2,54 @@ import * as PIXI from 'pixi.js'
 import SOUND from 'pixi-sound'
 import config from './config'
 import setup from './setup'
+import * as WebFont from 'webfontloader'
 
-const app = new PIXI.Application(config)
+const FONT = 'Skranji'
+WebFont.load({
+  google: {
+    families: [FONT]
+  },
+  fontloading: () => {
+    var el = document.createElement('p');
+	  el.style.fontFamily = FONT;
+	  el.style.fontSize = "0";
+	  el.style.visibility = "hidden";
+	  el.innerHTML = '.';
+	
+	  document.body.appendChild(el);
+  },
+  active: () => setTimeout(start, 100)
+})
 
-document.body.appendChild(app.view)
+function start() {
+  const app = new PIXI.Application(config)
 
-SOUND.Sound.from({
-  url: 'assets/music.mp3',
-  autoPlay: true,
-  loop: true
-});
+  document.body.appendChild(app.view)
 
-app.loader
-  .add('assets/campfire/frame-001.png')
-  .add('assets/campfire/frame-002.png')
-  .add('assets/campfire/frame-003.png')
-  .add('assets/campfire/frame-004.png')
-  .add('assets/explosion/frame-001.png')
-  .add('assets/explosion/frame-002.png')
-  .add('assets/explosion/frame-003.png')
-  .add('assets/explosion/frame-004.png')
-  .add('assets/ground.png')
-  .add('assets/bucket/frame-001.png')
-  .add('assets/bucket/frame-002.png')
-  .add('assets/logs/frame-001.png')
-  .add('assets/logs/frame-002.png')
-  .add('assets/bottle/frame-001.png')
-  .add('assets/bottle/frame-002.png')
-  .add('assets/logs.png')
-  .add('assets/plank.png')
-  .add('assets/plank-large.png')
-  .load(setup(app.stage, app.renderer, app.ticker))
+  SOUND.Sound.from({
+    url: 'assets/music.mp3',
+    autoPlay: true,
+    loop: true
+  });
+
+  app.loader
+    .add('assets/campfire/frame-001.png')
+    .add('assets/campfire/frame-002.png')
+    .add('assets/campfire/frame-003.png')
+    .add('assets/campfire/frame-004.png')
+    .add('assets/explosion/frame-001.png')
+    .add('assets/explosion/frame-002.png')
+    .add('assets/explosion/frame-003.png')
+    .add('assets/explosion/frame-004.png')
+    .add('assets/ground.png')
+    .add('assets/bucket/frame-001.png')
+    .add('assets/bucket/frame-002.png')
+    .add('assets/logs/frame-001.png')
+    .add('assets/logs/frame-002.png')
+    .add('assets/bottle/frame-001.png')
+    .add('assets/bottle/frame-002.png')
+    .add('assets/logs.png')
+    .add('assets/plank.png')
+    .add('assets/plank-large.png')
+    .load(setup(app.stage, app.renderer, app.ticker))
+}
